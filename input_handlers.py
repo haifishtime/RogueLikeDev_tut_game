facf5f0fafcf1a52530498d5aaa2904132458060
Felix <felixoehler@yahoo.de>
2021-10-25 13:43:07 +0200

    refactor

--- input_handlers.py
+++ input_handlers.py
@@ -1,11 +1,30 @@
-from typing import Optional
+from __future__ import annotations
+
+from typing import Optional, TYPE_CHECKING
 
 import tcod.event
 
 from actions import Action, EscapeAction, BumbAction
 
+if TYPE_CHECKING:
+    from engine import Engine
+
 
 class EventHandler(tcod.event.EventDispatch[Action]):
+    def __init__(self, engine: Engine):
+        self.engine = engine
+
+    def handle_events(self) -> None:
+        for event in tcod.event.wait():
+            action = self.dispatch(event)
+
+            if action is None:
+                continue
+
+            action.perform()
+            self.engine.handle_enemy_turns()
+            self.engine.update_fov()  # Update FOV before the players next action
+
     def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
         raise SystemExit()
 
@@ -14,16 +33,17 @@ class EventHandler(tcod.event.EventDispatch[Action]):
 
         key = event.sym
 
+        player = self.engine.player
         if key == tcod.event.K_UP:
-            action = BumbAction(dx=0, dy=-1)
+            action = BumbAction(player, dx=0, dy=-1)
         elif key == tcod.event.K_DOWN:
-            action = BumbAction(dx=0, dy=1)
+            action = BumbAction(player, dx=0, dy=1)
         elif key == tcod.event.K_LEFT:
-            action = BumbAction(dx=-1, dy=0)
+            action = BumbAction(player, dx=-1, dy=0)
         elif key == tcod.event.K_RIGHT:
-            action = BumbAction(dx=1, dy=0)
+            action = BumbAction(player, dx=1, dy=0)
 
         elif key == tcod.event.K_ESCAPE:
-            action = EscapeAction()
+            action = EscapeAction(player)
 
         return action
